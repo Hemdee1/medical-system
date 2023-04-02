9e8cfb098f16e8d4f69b258911af256c9169fe4c
hemdee <sanusi5110@gmail.com>
2023-04-02 22:17:36 +0100

    initial commit

---
+++ components/Welcome.js
@@ -0,0 +1,9 @@
+const Welcome = () => {
+  return (
+    <h2 className="text-2xl font-bold font-OpenSans tracking-wider mt-5">
+      Welcome Team Tropical Treatment <span className="text-6xl">🚀</span>
+    </h2>
+  );
+};
+
+export default Welcome;
