a744c801ae4943a8398e746aa7764bd712a396dc
ABDULFATTAH OLADAYO <sanusifatah16@gmail.com>
2023-04-22 22:22:33 +0100

    update config

--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ const nextConfig = {
       {
         source: "/api/:path*",
         // destination: "http://localhost:5000/api/:path*",
-        destination:
-          "https://medical-system-backend-production.up.railway.app/api/:path*",
+        // destination:
+        //   "https://medical-system-backend-production.up.railway.app/api/:path*",
+        destination: "https://medical-system.onrender.com/api/:path*",
       },
     ];
   },
